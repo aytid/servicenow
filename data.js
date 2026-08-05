1679b3ecdd14eca84cc32ad96285e4eebfef836f
Rohan Aditya <adityathirumala20@gmail.com>
2026-08-05 18:29:39 +0530

    Update data.js

--- data.js
+++ data.js
@@ -3042,7 +3042,7 @@ This enhancement extends the native Provider Notification framework while giving
 },
       {
     id: '1015',
-    title: 'Customizing Microsoft Teams Collaboration Chat with Direct Record Links',
+    title: 'Customizing Microsoft Teams Collaboration Chat with Record Links',
     category: 'Tutorial',
     excerpt: 'Enhance the default Microsoft Teams Collaboration Chat experience by adding direct links to ServiceNow records for both Employee Center and Agent Workspace.',
     tags: ['Microsoft Teams', 'Collaboration Chat', 'Script Include', 'ServiceNow', 'Employee Center', 'Agent Workspace'],
